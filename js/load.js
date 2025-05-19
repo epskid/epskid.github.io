5bb62520169a9cb4aeb32e1dee4b6cb545f41246
chord <None>
2025-05-18 19:42:14 -0600

    add projects

--- js/load.js
+++ js/load.js
@@ -5,11 +5,11 @@ const welcome = document.getElementById("welcome")
 
 let i = 1
 text.split('\n').forEach(line => {
-	if ((line.length !== 0) && (line !== '\t')) {
-		setTimeout(() => {
-			welcome.innerText += line.slice(2) + "\n"
-		}, (i++ + Math.random()) * 100)
-	} 
+  if ((line.length !== 0) && (line !== '\t')) {
+    setTimeout(() => {
+      welcome.innerText += line.slice(2) + "\n"
+    }, (i++ + Math.random()) * 100)
+  } 
 })
 
 setTimeout(loadShell, (i + 1) * 100)
--- js/shell.js
+++ js/shell.js
@@ -17,81 +17,96 @@ promptLine.appendChild(input)
 shell.appendChild(promptLine)
 
 input.onkeyup = ev => {
-	if (ev.keyCode === 13) {
-		const raw = input.value
-		input.style.display = "none"
-		appendPrev(`$ ${raw}`)
-		runIt(raw)
-		input.value = ""
-		input.style.display = "initial"
-		input.scrollIntoView({ block: "end" })
-	}
+  if (ev.keyCode === 13) {
+    const raw = input.value
+    input.style.display = "none"
+    appendPrev(`$ ${raw}`)
+    runIt(raw)
+    input.value = ""
+    input.style.display = "initial"
+    input.scrollIntoView({ block: "end" })
+  }
 }
 
 commands = {
-	help: [function() {
-		appendPrev("shkid: epskidOS shell")
-		appendPrev("here are the available commands:")
+  help: [function() {
+    appendPrev("shkid: epskidOS shell")
+    appendPrev("here are the available commands:")
 
-		for ([command, value] of Object.entries(commands)) {
-			appendPrev(` * ${command} -- ${value[1]}`)
-		}
-	}, "get help"],
-	clear: [function() {
-		welcome.innerText = ""
-		prev.innerText = ""
-	}, "clear the screen"],
-	github: [function() {
-		window.open("https://github.com/epskid", '_blank').focus()
-	}, "open github.com/epskid in a new tab"],
-	whoami: [function() {
-		appendPrev("i'm epskid")
-		appendPrev("i do stuff with computers sometimes")
-		appendPrev("being anonymous on the internet requires that online descriptions must be as vague as possible")
-	}, "who am i?"],
-	neofetch: [function() {
-	 	appendPrev(
+    for ([command, value] of Object.entries(commands)) {
+      appendPrev(` * ${command} -- ${value[1]}`)
+    }
+  }, "get help"],
+  clear: [function() {
+    welcome.innerText = ""
+    prev.innerText = ""
+  }, "clear the screen"],
+  github: [function() {
+    window.open("https://github.com/epskid", '_blank').focus()
+  }, "open github.com/epskid in a new tab"],
+  whoami: [function() {
+    appendPrev("i'm epskid")
+    appendPrev("i do stuff with computers sometimes")
+    appendPrev("being anonymous on the internet requires that online descriptions must be as vague as possible")
+  }, "who am i?"],
+  neofetch: [function() {
+     appendPrev(
 `     ___     epskid@browser
     / _ \\    OS: epskidOS
    |  __/    SHELL: shkid
     \\___|    STATUS: epic
    
 `)
-	}, "mandatory command on unix systems"]
+  }, "mandatory command on unix systems"],
+  project: [function(project) {
+    const projects = {
+      pie: "a python \"ide\""
+    }
+
+    if (project === "list") {
+      for ([p, desc] of Object.entries(projects)) {
+        appendPrev(` * ${p} -- ${desc}`)
+      }
+    } else if (projects[project]) {
+      window.location = project
+    } else {
+      appendPrev(`no such project: \`${project}\` (pass \`list\` for a list)`)
+    }
+  }, "open a project (pass `list` to list projects)"]
 }
 
 function runIt(raw) {
-	const lexed = raw.split(/[ \t]+/);
-	const command = lexed[0]
-	if (command === "") {
-		return;
-	}
-	const args = lexed.slice(1)
+  const lexed = raw.split(/[ \t]+/);
+  const command = lexed[0]
+  if (command === "") {
+    return;
+  }
+  const args = lexed.slice(1)
 
-	if (command in commands) {
-		if (commands[command][0].length === args.length) {
-			commands[command][0](...args)
-		} else {
-			appendPrev(`command \`${command}\` takes ${commands[command][0].length} argument(s) (passed ${args.length})`)
-		}
-	} else {
-		appendPrev(`command \`${command}\` not found -- run \`help\` for a list of commands`)
-	}
+  if (command in commands) {
+    if (commands[command][0].length === args.length) {
+      commands[command][0](...args)
+    } else {
+      appendPrev(`command \`${command}\` takes ${commands[command][0].length} argument(s) (passed ${args.length})`)
+    }
+  } else {
+    appendPrev(`command \`${command}\` not found -- run \`help\` for a list of commands`)
+  }
 }
 
 function appendPrev(text) {
-	const shline = document.createElement("div")
-	shline.innerText = text
-	prev.appendChild(shline)
+  const shline = document.createElement("div")
+  shline.innerText = text
+  prev.appendChild(shline)
 }
 
 function loadShell() {
-	document.body.appendChild(shell)
-	input.focus()
-	window.onfocus = _ => { input.focus() }
-	window.onclick = _ => { input.focus() }
-	setTimeout(_ => {
-		appendPrev("shkid initialized")
-		appendPrev("type `help` for help")
-	}, 100)
+  document.body.appendChild(shell)
+  input.focus()
+  window.onfocus = _ => { input.focus() }
+  window.onclick = _ => { input.focus() }
+  setTimeout(_ => {
+    appendPrev("shkid initialized")
+    appendPrev("type `help` for help")
+  }, 100)
 }
